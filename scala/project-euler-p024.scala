df44e370861e1e72ae4b9327ac2056539fb4d7a6
Saran Tumnee <stumnee@gmail.com>
2018-05-09 16:53:45 -0700

    p24 solved lexicographic permutations at 1m using recursion on ArrayBuffer

--- scala/project-euler-p024.scala
+++ scala/project-euler-p024.scala
@@ -7,3 +7,47 @@
   *
   * https://projecteuler.net/problem=24
   **/
+
+import collection.mutable.ArrayBuffer
+
+val max = 1000000
+
+var (s, count) = ("", 0)
+
+/**
+  * Recursively generate permutations for a subset input
+  * @param a (1,2,3...)
+  */
+def perm(a: ArrayBuffer[Int]):Unit = {
+  var (idx, leadDigit) = (0, a(0))
+
+  if (a.length == 1) {
+    count += 1
+  } else {
+    while (idx < a.size && count < max) {
+      var b = a.clone()
+      leadDigit = b.remove(idx)
+
+      perm(b)
+      idx += 1
+    }
+
+  }
+
+  if (count == max) {
+    s = leadDigit + s
+  }
+
+}
+
+perm(ArrayBuffer(0,1,2,3,4,5,6,7,8,9))
+
+println(s)
+/*
+2783915460
+
+real    0m1.331s
+user    0m1.340s
+sys     0m0.152s
+
+ */
