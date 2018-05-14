c609e06a7665077710a49776d7a6f79c87e0283e
Saran Tumnee <stumnee@gmail.com>
2018-05-14 00:27:59 -0700

    p32 Pandigital products - loop with checking if the number is a pandigital

--- scala/project-euler-p032.scala
+++ scala/project-euler-p032.scala
@@ -7,3 +7,74 @@
   *
   * https://projecteuler.net/problem=32
   **/
+
+// 123456789
+
+// Not Possible
+// . x  . <= ..
+// .. x .. <= ....
+// ... x ... >= .....
+
+// Possible cases
+// ... x .. == ....
+// .... x . == ....
+
+val d = Array(1, 2, 3, 4, 5, 6, 7, 8, 9)
+
+def isPandigital(s: String): Boolean = {
+  if (s.indexOf("0") > -1) {
+    return false
+  }
+
+  for (i <- 0 until s.length - 1) {
+    if (s.indexOf(s(i), i + 1) > -1) {
+      return false
+    }
+  }
+  true
+}
+
+var products = collection.mutable.ListBuffer[Int]()
+
+for (multiplicand <- 123 to 9876) {
+
+  // for smaller set of multipliers,
+  // this check does not save execution time!
+  if (isPandigital(multiplicand.toString)) {
+    for (multiplier <- 1 to 98) {
+
+      val product = multiplicand * multiplier
+
+      val digits = multiplicand.toString +
+        multiplier.toString +
+        product.toString
+
+      if (digits.length == 9 && isPandigital(digits)) {
+
+        println(s"${multiplicand} x ${multiplier} = ${product}")
+
+        products += product
+
+      }
+    }
+  }
+}
+
+println(products.distinct.sum)
+/*
+138 x 42 = 5796
+157 x 28 = 4396
+159 x 48 = 7632
+186 x 39 = 7254
+198 x 27 = 5346
+297 x 18 = 5346
+483 x 12 = 5796
+1738 x 4 = 6952
+1963 x 4 = 7852
+45228
+
+real    0m1.524s
+user    0m1.976s
+sys     0m0.182s
+
+ */
