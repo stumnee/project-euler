6fce9aa8445141594b12727c34a37abfa619d0ce
Saran Tumnee <stumnee@gmail.com>
2018-05-22 00:15:10 -0700

    Problem 42. Coded triangle numbers

---
+++ scala/project-euler-p042.scala
@@ -0,0 +1,34 @@
+/**
+  * Problem 42. Coded triangle numbers
+  *
+  * The nth term of the sequence of triangle numbers is given by, tn = ½n(n+1); so the first ten triangle numbers are:
+  * 1, 3, 6, 10, 15, 21, 28, 36, 45, 55, ...
+  * By converting each letter in a word to a number corresponding to its alphabetical position and adding these values we form a word value. For example, the word value for SKY is 19 + 11 + 25 = 55 = t10. If the word value is a triangle number then we shall call the word a triangle word.
+  * Using words.txt (right click and 'Save Link/Target As...'), a 16K text file containing nearly two-thousand common English words, how many are triangle words?
+  *
+  * https://projecteuler.net/problem=42
+  **/
+
+// The last element is 5050,
+// should be enough for very long english word
+val triangleNums = (1 to 100).map(n=>n*(n+1)/2).toArray
+
+var count = 0
+
+io.Source.fromFile("p042_words.txt")
+  .mkString
+  .replaceAll("\"", "")
+  .split(",")
+  .foreach{word=>
+
+    val wordValue = word.toUpperCase()
+          .map(_ - 'A' + 1)
+          .sum
+
+    if (triangleNums.indexOf(wordValue) > -1) {
+     count += 1
+    }
+}
+
+println(count)
+
